3d2eb29da3d6aebf6b9f3c2e272b10e606892807
ubuntu <ubuntu@localhost.localdomain>
2021-09-11 23:28:59 +0800

    ch11

---
+++ ch11/3.c
@@ -0,0 +1,39 @@
+#include <pthread.h>
+
+struct msg{
+    struct msg *m_next;
+    struct msg *m_prev;
+    pthread_t tid;
+};
+
+struct msg *workq;
+
+pthread_cond_t qready = PTHREAD_COND_INITIALIZER;
+
+pthread_mutex_t qlock = PTHREAD_MUTEX_INITIALIZER;
+
+void queue_init(struct msg *head){
+    struct 
+}
+
+void process_msg(void) {
+    struct msg *mp;
+
+    for(;;) {
+        pthread_mutex_lock(&qlock);
+        while(workq == NULL)
+            pthread_cond_wait(&qready, &qlock);
+        mp = workq;
+        workq = mp->m_next;
+        pthread_mutex_unlock(&qlock);
+
+    }
+}
+
+void enqueue_msg(struct msg *mp){
+    pthread_mutex_lock(&qlock);
+    mp->m_next = workq;
+    workq = mp;
+    pthread_mutex_unlock(&qlock);
+    pthread_cond_signal(&qready);
+}
